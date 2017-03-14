96219743a8e633fb262ced11a442b28bfcce62cb
Putthiphong Boonphong <putthiphong.boonphong@gmail.com>
2017-03-14 23:54:09 +0700

    Temporary fix incompatible type when pass Java class to Kotlin annotation.

--- src/main/kotlin/org/mybatis/spring/annotation/MapperScan.kt
+++ src/main/kotlin/org/mybatis/spring/annotation/MapperScan.kt
@@ -25,7 +25,6 @@ import kotlin.reflect.KClass
  * Use this annotation to register MyBatis mapper interfaces when using Java
  * Config. It performs when same work as [MapperScannerConfigurer] via
  * [MapperScannerRegistrar].
-
  *
  * Configuration example:
  *
@@ -65,18 +64,21 @@ import kotlin.reflect.KClass
 @MustBeDocumented
 @Import(MapperScannerRegistrar::class)
 annotation class MapperScan(
+
     /**
      * Alias for the [.basePackages] attribute. Allows for more concise
      * annotation declarations e.g.:
      * `@EnableMyBatisMapperScanner(&quot;org.my.pkg&quot;)` instead of `&quot;org.my.pkg&quot;`)}.
      */
     vararg val value: String = arrayOf(),
+
     /**
      * Base packages to scan for MyBatis interfaces. Note that only interfaces
      * with at least one method will be registered; concrete classes will be
      * ignored.
      */
     val basePackages: Array<String> = arrayOf(),
+
     /**
      * Type-safe alternative to [.basePackages] for specifying the packages
      * to scan for annotated components. The package of each class specified will be scanned.
@@ -85,11 +87,13 @@ annotation class MapperScan(
      * that serves no purpose other than being referenced by this attribute.
      */
     val basePackageClasses: Array<KClass<*>> = arrayOf(),
+
     /**
      * The [BeanNameGenerator] class to be used for naming detected components
      * within the Spring container.
      */
     val nameGenerator: KClass<out BeanNameGenerator> = BeanNameGenerator::class,
+
     /**
      * This property specifies the annotation that the scanner will search for.
      *
@@ -100,32 +104,37 @@ annotation class MapperScan(
      *
      * Note this can be combined with markerInterface.
      */
+
     val annotationClass: KClass<out Annotation> = Annotation::class,
     /**
      * This property specifies the parent that the scanner will search for.
      *
-     *
      * The scanner will register all interfaces in the base package that also have
      * the specified interface class as a parent.
-     *
      *
      * Note this can be combined with annotationClass.
      */
     val markerInterface: KClass<*> = Class::class,
+
     /**
      * Specifies which `SqlSessionTemplate` to use in the case that there is
      * more than one in the spring context. Usually this is only needed when you
      * have more than one datasource.
      */
     val sqlSessionTemplateRef: String = "",
+
     /**
      * Specifies which `SqlSessionFactory` to use in the case that there is
      * more than one in the spring context. Usually this is only needed when you
      * have more than one datasource.
      */
     val sqlSessionFactoryRef: String = "",
+
     /**
      * Specifies a custom MapperFactoryBean to return a mybatis proxy as spring bean.
      */
-    val factoryBean: KClass<out MapperFactoryBean<*>> = MapperFactoryBean::class
+// FIXME: Incompatible types when pass Java class to Kotlin annotation
+// @see https://discuss.kotlinlang.org/t/how-to-pass-java-class-as-parameter-to-kotlin-annotation/2374
+//    val factoryBean: KClass<out MapperFactoryBean<*>> = MapperFactoryBean::class
+    val factoryBean: KClass<*> = MapperFactoryBean::class
 )
